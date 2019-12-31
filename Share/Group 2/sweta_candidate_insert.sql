2ea1712c9033f8ae80edd437f0c103b07a093a19
sweta096 <59357963+sweta096@users.noreply.github.com>
2019-12-31 14:45:21 +0530

    Add files via upload

---
+++ Share/Group 2/sweta_candidate_insert.sql
@@ -0,0 +1,19 @@
+insert queries
+
+
+
+insert into candidate_master(candidate_id,name,location,contact,email,qualification,
+skills,certification,status,created_on,updated_on)values(1002, 'sweta', 'mumbai', '1234567890', 'abc@gmail.com', 'BE', 'JAVA', 'java certified', 'selected', TO_DATE('2019-11-15 11:12:34','YYYY-MM-DD HH:MI:SS'), TO_DATE('2019-11-15 05:12:34','YYYY-MM-DD HH:MI:SS'));
+
+
+
+insert into candidate_master(candidate_id,name,location,contact,email,qualification,
+skills,certification,status,created_on,updated_on)values(1003, 'chandan', 'thane', '1298463740', 'xyz@gmail.com', 'mca', '.net', 'java certified', 'selected', TO_DATE('2019-11-12 11:12:34','YYYY-MM-DD HH:MI:SS'), TO_DATE('2019-11-12 05:12:34','YYYY-MM-DD HH:MI:SS'));
+
+
+insert into candidate_master(candidate_id,name,location,contact,email,qualification,
+skills,certification,status,created_on,updated_on)values(1004, 'siddhesh', 'goa', '4653837253', 'pqr@gmail.com', 'be', '.java', 'java certified', 'selected', TO_DATE('2019-11-24 11:12:34','YYYY-MM-DD HH:MI:SS'), TO_DATE('2019-11-24 05:12:34','YYYY-MM-DD HH:MI:SS'));
+
+
+insert into candidate_master(candidate_id,name,location,contact,email,qualification,
+skills,certification,status,created_on,updated_on)values(1005, 'vaishnavi', 'alibaug', '1736458974', 'vai@gmail.com', 'be', 'java', 'java certified', 'selected', TO_DATE('2019-11-12 11:12:34','YYYY-MM-DD HH:MI:SS'), TO_DATE('2019-11-12 05:12:34','YYYY-MM-DD HH:MI:SS'));
